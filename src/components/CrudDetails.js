7994261d39c05aa49f2a8c8f7fe48e80d88ac110
Govind Jangra <govind70821@gmail.com>
2023-01-20 02:39:59 +0530

    Dynamic map added

--- src/components/CrudDetails.js
+++ src/components/CrudDetails.js
@@ -3,7 +3,16 @@ import AddContact from './AddContact';
 import ContactList from './ContactList';
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-
+import Mapkkr from './map places/Mapkkr';
+import MapBus from './map places/MapBus';
+import MapDivine from './map places/MapDivine';
+import MapIskon from './map places/MapIskon';
+import MapJyotisar from './map places/MapJyotisar';
+import Mapkesal from './map places/Mapkesal';
+import MapPeepli from './map places/MapPeepli';
+import MapStation from './map places/MapStation';
+import Mapzoo from './map places/Mapzoo';
+import MapReturn from './MapReturn';
 const CrudDetails = () => {
   
     const [contacts, setContacts] = useState([]);
@@ -37,10 +46,50 @@ const CrudDetails = () => {
         const selected = contacts.find((c) => c.id === id);
         setSelectedContact(selected);
       };
+      // const maps = {
+      //   "Mapkkr": <Mapkkr />,
+      //   "MapBus": <MapBus />,
+      //   "MapDivine": <MapDivine />,
+      //   "MapIskon": <MapIskon />,
+      //   "MapJyotisar": <MapJyotisar />,
+      //   "Mapkesal": <Mapkesal />,
+      //   "MapPeepli": <MapPeepli />,
+      //   "MapStation": <MapStation />,
+      //   "Mapzoo": <Mapzoo />
+      //   }
+
+      const Map = (props) => {
+        if (props.id === 'kkr') {
+          return <Mapkkr />
+        } else if (props.id === 'Bus') {
+          return <MapBus />
+        } else if (props.id === 'Divine') {
+          return <MapDivine />
+        } else if (props.id === 'Iskon') {
+          return <MapIskon />
+        } else if (props.id === 'Jyotisar') {
+          return <MapJyotisar />
+        } else if (props.id === 'kesal') {
+          return <Mapkesal />
+        } else if (props.id === 'Peepli') {
+          return <MapPeepli />
+        } else if (props.id === 'Station') {
+          return <MapStation />
+        } else if (props.id === 'zoo') {
+          return <Mapzoo />
+        } else {
+          return <div>Map not found: {props.id}</div>
+        }
+      }
       let {id} = useParams();  //to retain last path name in address
   return (
     <>
     
+    {/* <div>
+    {maps[id]}
+    
+    </div> */}
+    <Map id={id}/>
       <ContactList  id={id}
         contacts={contacts}
         deleteContact={deleteContact}
